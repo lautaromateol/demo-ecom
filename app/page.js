a5bc42e7f69a236459868a212d122a3d458c5c7d
lautaromateol <lautaromateol@gmail.com>
2024-01-11 21:43:29 -0300

    products cards added to homepage

--- app/page.js
+++ app/page.js
@@ -1,13 +1,35 @@
-import Image from 'next/image'
+import Pagination from "@/components/product/Pagination";
+import ProductCard from "@/components/product/ProductCard";
 
-export default function Home() {
-  return (
-   <main>
-    <div>
-      <h1 className='flex items-center justify-center uppercase min-h-screen'>
-        Home
-      </h1>
-    </div>
-   </main>
-  )
+async function getProducts(searchParams) {
+
+  const { page } = searchParams || 1
+
+  const response = await fetch(`${process.env.API}/product?page=${page}`,
+    {
+      method: "GET",
+      next: { revalidate: 1 }
+    })
+  const data = await response.json()
+
+  return data
 }
+
+export default async function Home({ searchParams }) {
+
+  const { products, totalPages, currentPage } = await getProducts(searchParams)
+
+  return (
+    <main>
+      <h1 className="text-3xl uppercase font-bold text-center">
+        Last Products
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 my-10">
+        {products?.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div>
+      <Pagination totalPages={totalPages} currentPage={currentPage} />
+    </main>
+  );
+} 
