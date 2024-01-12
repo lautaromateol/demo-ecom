7bedc7bb359c387a055a835cadace81903c56ffa
lautaromateol <lautaromateol@gmail.com>
2024-01-11 21:50:19 -0300

    new env variable

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 const config = require('./config')
 
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com'
+            }
+        ]
+    },
     env: {
         DB_URI: config.DB_URI,
         API: config.API,
         NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
         GOOGLE_CLIENT_ID: config.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: config.GOOGLE_CLIENT_SECRET
+        GOOGLE_CLIENT_SECRET: config.GOOGLE_CLIENT_SECRET,
+        CLOUDINARY_CLOUD_NAME: config.CLOUDINARY_CLOUD_NAME,
+        CLOUDINARY_API_KEY: config.CLOUDINARY_API_KEY,
+        CLOUDINARY_API_SECRET: config.CLOUDINARY_API_SECRET
     },
 };
 
