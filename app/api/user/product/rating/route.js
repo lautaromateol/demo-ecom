8dfcdb019e789c460b85663bd61e6c286fe09cac
lautaromateol <lautaromateol@gmail.com>
2024-01-18 19:59:18 -0300

    like and unlike routes created

---
+++ app/api/user/product/rating/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/utils/dbConnect";
+import Product from "@/models/product";
+// import Order from "@/models/order";
+import { currentUser } from "@/utils/currentUser";
+
+export async function POST(req){
+    await dbConnect()
+    const { productId, rating, comment } = await req.json()
+    const user = await currentUser(req)
+
+    try {
+        const product = await Product.findById(productId)
+        const existingRating = product.ratings.find((rate) => rate.postedBy.toString() === user._id.toString())
+        // const userPurchased = await Order.findOne({
+        //     userId: user._id,
+        //     "cartItems._id": productId
+        // })
+        // if(!userPurchased) {
+        //     return NextResponse.json({ error: "You can only leave reviews to products you have purchased"}, { status: 400 })
+        // }
+        if(existingRating){
+            existingRating.rating = rating
+            existingRating.comment = comment
+            await product.save()
+            return NextResponse.json(product, { status: 200 })
+        }
+        product.ratings.push({
+            rating,
+            comment,
+            postedBy: user._id
+        })
+        const updated = await product.save()
+        return NextResponse.json(updated, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
--- app/api/user/product/unlike/route.js
+++ app/api/user/product/unlike/route.js
@@ -1,17 +1,14 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/utils/dbConnect";
-import Product from "@/model/product";
-import { getToken } from "next-auth/jwt";
+import Product from "@/models/product";
+import { currentUser } from "@/utils/currentUser";
 
 export async function PUT(req) {
     await dbConnect()
 
     const { productId } = await req.json()
 
-    const { user } = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET
-    })
+    const user = await currentUser()
 
     try {
         const updated = await Product.findByIdAndUpdate(productId,
