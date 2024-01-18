e737a03b92c4af02c80ed337b21e82d76adb33ab
lautaromateol <lautaromateol@gmail.com>
2024-01-18 20:01:38 -0300

    shop and product search results page created

---
+++ app/search/product/page.jsx
@@ -0,0 +1,50 @@
+"use client"
+import ProductCard from "@/components/product/ProductCard";
+import { useProductContext } from "@/context/ProductContext";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const SearchResults = () => {
+
+    const { productSearchResults, setProductSearchResults } = useProductContext()
+
+    const searchParams = useSearchParams()
+
+    const query = searchParams.get("productSearchQuery")
+
+    const fetchProductSearchResultsReload = async () => {
+        try {
+            const response = await fetch(`${process.env.API}/product/search?title=${query}`, {
+                method: "GET"
+            })
+            const data = await response.json()
+            if (response.ok) {
+                setProductSearchResults(data)
+            } else {
+                toast.error(data.error)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchProductSearchResultsReload()
+    }, [query])
+
+    return (
+        <div className="container">
+            <div className="mx-auto max-w-5xl p-4">
+                <h4 className="text-xl font-bold uppercase">Search results: {productSearchResults?.length}</h4>
+                <hr className="w-full mt-2" />
+                <div className="grid grid-cols-3">
+                    {productSearchResults?.map((product) => (
+                        <ProductCard key={product?._id} product={product} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchResults;
---
+++ app/shop/page.jsx
@@ -0,0 +1,51 @@
+import ProductCard from "@/components/product/ProductCard";
+import ProductFilter from "@/components/product/ProductFilter";
+
+async function getProducts(searchParams) {
+    const searchQuery = new URLSearchParams({
+        page: searchParams.page || 1,
+        minPrice: searchParams.minPrice || "",
+        maxPrice: searchParams.maxPrice || "",
+        ratings: searchParams.ratings || "",
+        category: searchParams.category || "",
+        tag: searchParams.tag || "",
+        brand: searchParams.brand || ""
+    }).toString()
+
+    try {
+        const response = await fetch(`${process.env.API}/product/filters?${searchQuery}`, {
+            method: "GET"
+        })
+        const data = await response.json()
+        if (!response.ok) throw new Error("Failed to fetch products")
+        if(!data || !Array.isArray(data.products)) throw new Error("No products returned")
+        return data
+    } catch (error) {
+        console.log(error.message)
+        return { products: [], currentPage: 1, totalPages: 1 }
+    }
+}
+
+export default async function Shop({ searchParams }) {
+    
+    const { products } = await getProducts(searchParams)
+
+    return (
+        <div className="container mx-auto">
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/4 overflow-auto max-h-[90vh]">
+                    <ProductFilter searchParams={searchParams} />
+                </div>
+                <div className="w-full lg:w-3/4 p-4">
+                    <h1 className="text-3xl font-bold text-center uppercase">All products</h1>
+                    <div className="grid grid-cols-3">
+                    {products?.map((product)=> (
+                        <ProductCard product={product}/>
+                        ))}
+                        </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
