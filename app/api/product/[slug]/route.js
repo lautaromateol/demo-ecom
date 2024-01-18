b01c445111f9fa1e4fa76004fe7ca866b735c182
lautaromateol <lautaromateol@gmail.com>
2024-01-18 19:59:51 -0300

    properties added to product slug API route

--- app/api/product/[slug]/route.js
+++ app/api/product/[slug]/route.js
@@ -1,15 +1,33 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/utils/dbConnect";
 import Product from "@/models/product";
+import queryString from "query-string";
 
 export async function GET(req, {params}){
     await dbConnect()
+    const { edition } = queryString.parseUrl(req.url).query || ""
 
     try {
        const product = await Product.findOne({
         slug: params.slug
        })
-       return NextResponse.json(product, { status: 200 }) 
+       .populate({
+        path: "ratings.postedBy",
+        model: "User",
+        select: "name"
+       })
+       if(!edition) {
+        const selectedEdition = product.editions[0]
+        return NextResponse.json({
+            product,
+            selectedEdition
+           }, { status: 200 }) 
+       }
+       const selectedEdition = product.editions.filter((el) => el.slug === edition)[0]
+       return NextResponse.json({
+        product,
+        selectedEdition
+       }, { status: 200 }) 
     } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 200 })
     }
