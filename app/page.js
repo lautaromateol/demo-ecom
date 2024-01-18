f70d7dcaa7c41b45a33b6b3fa994eb84fbdaaac5
lautaromateol <lautaromateol@gmail.com>
2024-01-18 19:53:46 -0300

    website metadata added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import Pagination from "@/components/product/Pagination";
 import ProductCard from "@/components/product/ProductCard";
 
+export const metadata = {
+  title: "Next Ecommerce",
+  description: "Find all the videogames you are searching for in this videogame"
+}
+
 async function getProducts(searchParams) {
 
   const { page } = searchParams || 1
@@ -26,7 +31,7 @@ export default async function Home({ searchParams }) {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 my-10">
         {products?.map((product) => (
-          <ProductCard product={product} />
+          <ProductCard key={product?._id} product={product} />
         ))}
       </div>
       <Pagination totalPages={totalPages} currentPage={currentPage} />
