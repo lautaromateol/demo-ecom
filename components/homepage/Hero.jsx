dc4053c32dbe9c7d404ddb86027c71b1f62e5aca
lautaromateol <lautaromateol@gmail.com>
2024-06-04 17:06:19 -0300

    link prop fixed

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
           <h1 className='text-5xl lg:text-6xl text-primary leading-tight font-medium mb-6'>Give your home a feeling of unique exclusivity</h1>
           <p className='text-xl leading-normal text-secondary mb-8'>Discover our wide variety of furniture that we have to fill your home with luxury.</p>
           <div className="flex gap-2">
-            <Link to="/shop" className='px-4 py-2 bg-main text-white rounded-full transition-all hover:bg-tint'>
+            <Link href="/shop" className='px-4 py-2 bg-main text-white rounded-full transition-all hover:bg-tint'>
               Buy Now
             </Link>
           </div>
