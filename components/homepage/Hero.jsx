e5b6082645b8c7f9e6be680195fc3bfd31dca715
lautaromateol <lautaromateol@gmail.com>
2024-06-04 17:02:54 -0300

    url added to hero button

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Hero() {
   return (
     <section className="mx-auto min-h-screen py-18 px-6 lg:px-0">
@@ -6,12 +8,9 @@ export default function Hero() {
           <h1 className='text-5xl lg:text-6xl text-primary leading-tight font-medium mb-6'>Give your home a feeling of unique exclusivity</h1>
           <p className='text-xl leading-normal text-secondary mb-8'>Discover our wide variety of furniture that we have to fill your home with luxury.</p>
           <div className="flex gap-2">
-            <button className='px-4 py-2 bg-main text-white rounded-full transition-all hover:bg-tint'>
+            <Link to="/shop" className='px-4 py-2 bg-main text-white rounded-full transition-all hover:bg-tint'>
               Buy Now
-            </button>
-            <button className="px-4 py-2 transition-all border-main hover:border-b hover:text-main">
-              See more &darr;
-            </button>
+            </Link>
           </div>
         </div>
         <img
